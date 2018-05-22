b802a0234c330edef99be000a3b50c96262f73cb
St@t1c_V01D <kai.schmitz1@rwth-aachen.de>
2018-05-22 17:03:52 +0200

    Projektdateien hinzufügen.

---
+++ I2C_Nano/I2C_Nano.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		I2C_Nano.ino
+ Created:	22.05.2018 17:03:43
+ Author:	Kai
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
